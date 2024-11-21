091e00fe12e12968f6a8bafbc3efd1a2cfd38df8
OmarE14 <omar.elanany@ontariotechu.net>
2024-11-21 12:05:19 -0500

    fixed routes

--- server/config/app.js
+++ server/config/app.js
@@ -7,7 +7,7 @@ let logger = require('morgan');
 let app = express();
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
-let bookRouter = require('../routes/book');
+let contactRouter = require('../routes/contact');
 
 
 // view engine setup
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/bookslist',bookRouter);
+app.use('/contactslist',contactRouter);
 // /project --> projectrouter
 // /contactus --> contactus
 
